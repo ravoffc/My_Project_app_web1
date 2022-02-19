4fdcc482aa330758b19eaae39a0ad45d55addca2
ravoffc <ravimuhps907@gmail.com>
2022-02-19 10:32:15 +0700

    initial commit

--- components/about.vue
+++ components/about.vue
@@ -1,6 +1,6 @@
 <template>
     <blockquote class="blockquote text-center">
         <p class="mb-0">We provide the best selection of products for anime lovers.</p>
-        <footer class="blockquote-footer">happy shopping and <cite title="Source Title">hopefully what you want is available if you can't contact us</cite></footer>
+        <footer class="blockquote-footer">happy shopping and <cite title="Source Title">hopefully what you want is available if not, you can contact us</cite></footer>
     </blockquote>
 </template>
